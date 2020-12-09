5e49d4937315491c7ed9123bd5629729e9255983
MUHIGIRI Ashuza <ashuzamh@gmail.com>
2020-12-09 17:53:58 +0200

    making some changes

---
+++ git-test.c
@@ -0,0 +1,23 @@
+#include <sdtio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(void)
+{
+    int x = 34;
+    int y = 50;
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+
+    // swapping ..
+    swap(&x, &y)
+
+        printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
